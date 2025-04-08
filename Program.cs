3d1c76c8a9442f24fcd5f668b055c8016a70feb3
Mathes Henrique Moreira <matheushmoreira695@gmail.com>
2025-04-08 11:41:15 -0300

    Início da funcionalidade

--- Program.cs
+++ Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main()
+    {
+        int ingressoVIP = 'x', ingressoPrioritario = 'y', ingressoComum = 'z', ingressoTotal = ingressoComum + ingressoPrioritario + ingressoVIP, controlador;
+
+        do{
+            System.Console.WriteLine("---------------------------------------");
+            System.Console.WriteLine("1. Registrar a entrada de um espectador");
+            System.Console.WriteLine("2. Registrar a saída de um espectador");
+            System.Console.WriteLine("3. Consultar ingressos disponíveis");
+            System.Console.WriteLine("4. Exibir resumo");
+            System.Console.WriteLine("5. Sair");
+            System.Console.WriteLine("---------------------------------------");
+            
+            int opcao = int.Parse(Console.ReadLine());
+            controlador = opcao;
+
+            switch(opcao){
+                case 1:
+                    break;
+                
+                case 2:
+                    break;
+
+                case 3:
+                    break;
+                
+                case 4:
+                    break;
+
+                case 5:
+                    break;
+                
+                default:
+                    Console.Clear();
+                    System.Console.WriteLine("-----------------------------");
+                    System.Console.WriteLine("A opção que digitou não exite \nConfira se digitou certo");
+                    System.Console.WriteLine("-----------------------------");
+                    System.Console.WriteLine("Aperte enter para voltar");
+                    System.Console.WriteLine("-----------------------------");
+                    Console.ReadLine();
+                    Console.Clear();
+
+                    break;
+            }
+        } while(controlador != 5);
+    }
+}
